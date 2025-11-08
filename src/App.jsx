aaaa9583ac741516f8980088679c75f644f8e05c
darya <dhaghighatpanah@gmail.com>
2025-11-08 13:03:26 +0330

    slide3 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Components/Header"
 import Slide1 from "./Components/Slide1"
 import Slide2 from "./Components/Slide2"
+import Slide3 from "./Components/Slide3"
 
 function App(){
   return(
@@ -8,6 +9,7 @@ function App(){
     <Header/>
     <Slide1/>
     <Slide2/>
+    <Slide3/>
     </>
   )
 }
