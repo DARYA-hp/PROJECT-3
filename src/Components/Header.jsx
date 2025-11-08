f50ab1e2986003ed583658426aaa40dd291dfb10
darya <dhaghighatpanah@gmail.com>
2025-11-08 10:20:58 +0330

    header uptadted

---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+function Header(){
+    return(
+        <>
+        <div className=" flex flex-row  justify-between px-[75px] py-7 ">
+            <div>
+                <img src="/Logo (1).png" alt="" />
+            </div>
+            <div className=" flex  flex-row justify-center items-center gap-12">
+                <ul className="flex gap-20 text-[16px] font-[600]">
+                    <li>HOME</li>
+                    <li>MARKET</li>
+                    <li>ABOUT US</li>
+                    <li>CONTACT US</li>
+                </ul>
+                <button className=" text-[16px] font-[600] text-[#1DAEFF] border-2 border-[#1DAEFF] py-[7px] px-[32px] rounded-xl">LOG IN</button>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Header
