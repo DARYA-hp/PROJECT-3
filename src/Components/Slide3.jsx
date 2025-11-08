8878b8adba7b8f73a3bb49e2ff598765752dd366
darya <dhaghighatpanah@gmail.com>
2025-11-08 20:53:56 +0330

    id1 had update

--- src/Components/Slide3.jsx
+++ src/Components/Slide3.jsx
@@ -1,11 +1,11 @@
 function Slide3() {
     const properties = [
         {id:1 , img: "/13.jpg", title: "103/143 West Street, Crows Nest", bedrooms: 10, area: 150, garage: 2, builder: "X Builder", price: "$45,545" },
-        {id: 2, img: "/14.jpg", title: "103/143 West Street, Crows Nest", bedrooms: 10, area: 150, garage: 2, builder: "X Builder", price: "$45,545" },
-        {id:3, img: "/15.jpg", title: "103/143 West Street, Crows Nest", bedrooms: 10, area: 150, garage: 2, builder: "X Builder", price: "$45,545" },
-        {id:4, img: "/16.jpg", title: "103/143 West Street, Crows Nest", bedrooms: 10, area: 150, garage: 2, builder: "X Builder", price: "$45,545" },
-        {id:5, img: "/17.jpg", title: "103/143 West Street, Crows Nest", bedrooms: 10, area: 150, garage: 2, builder: "X Builder", price: "$45,545" },
-        {id:6, img: "/18.jpg", title: "103/143 West Street, Crows Nest", bedrooms: 10, area: 150, garage: 2, builder: "X Builder", price: "$45,545" },
+        {id:2 , img: "/14.jpg", title: "103/143 West Street, Crows Nest", bedrooms: 10, area: 150, garage: 2, builder: "X Builder", price: "$45,545" },
+        {id:3 , img: "/15.jpg", title: "103/143 West Street, Crows Nest", bedrooms: 10, area: 150, garage: 2, builder: "X Builder", price: "$45,545" },
+        {id:4 , img: "/16.jpg", title: "103/143 West Street, Crows Nest", bedrooms: 10, area: 150, garage: 2, builder: "X Builder", price: "$45,545" },
+        {id:5 , img: "/17.jpg", title: "103/143 West Street, Crows Nest", bedrooms: 10, area: 150, garage: 2, builder: "X Builder", price: "$45,545" },
+        {id:6 , img: "/18.jpg", title: "103/143 West Street, Crows Nest", bedrooms: 10, area: 150, garage: 2, builder: "X Builder", price: "$45,545" },
     ];
 
     return (
@@ -18,9 +18,23 @@ function Slide3() {
 
                 <div className="pt-14 flex flex-row flex-wrap justify-between px-28 gap-y-8">
                     {properties.map((property, index) => (
-                        <div key={index} className="flex-row bg-white pl-10 items-center border-[2.5px] border-[#DCDCEB] rounded-3xl w-[545px] h-[250px] flex">
-                            <div>
-                                <img src={property.img} alt="" className="object-cover rounded-2xl w-[162px] h-[192px]" />
+                        <div
+                            key={index}
+                            className={`flex-row bg-white pl-10 items-center border-[2.5px] rounded-3xl w-[545px] h-[250px] flex relative transition-all duration-300 ${
+                                property.id === 1
+                                    ? "border-[#1DAEFF] shadow-[0_4px_25px_#6b728040]" 
+                                    : "border-[#c3c3cc]"
+                            }`}
+                        >
+                            <div className="relative">
+                                <img
+                                    src={property.img}
+                                    alt=""
+                                    className="object-cover rounded-2xl w-[162px] h-[192px]"
+                                />
+                                {property.id === 1 && (
+                                    <div className="absolute inset-0 bg-[#0aceff55] rounded-2xl"></div> // لایه آبی روشن
+                                )}
                             </div>
                             <div className="pl-10 flex flex-col gap-4">
                                 <p className="w-[280px] text-[28px] font-[650]">{property.title}</p>
@@ -30,8 +44,12 @@ function Slide3() {
                                     <p>{property.garage} Garage</p>
                                 </div>
                                 <div className="flex flex-row items-center gap-x-8">
-                                    <p className="text-[17px] text-[#8F90A6] font-[520]">Posted by {property.builder}</p>
-                                    <button className="pl-3 text-white py-[6px] text-[17px] font-[520] rounded-xl px-5 bg-[#8F90A6]">{property.price}</button>
+                                    <p className="text-[17px] text-[#8F90A6] font-[520]">
+                                        Posted by {property.builder}
+                                    </p>
+                                    <button className="pl-3 text-white py-[6px] text-[17px] font-[520] rounded-xl px-5 bg-[#8F90A6]">
+                                        {property.price}
+                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -39,26 +57,26 @@ function Slide3() {
                 </div>
             </div>
 
-
             <div className="px-28 pt-28">
-                <div className=" rounded-3xl bg-gradient-to-b from-[#1DAEFF] to-[#ddeefc] h-[480px]   flex flex-row justify-between ">
-                    <div className=" pl-16 pt-20  ">
-                        <p className=" text-[54px] w-[400px]  font-[600]">Find your best Real Estate</p>
-                        <p className="pt-8 w-[470px] text-[22px] font-[400] text-[#585981]">We provide a complete service for the sale, purchase or rental of real estate.</p>
-                        <div className=" pt-8">
-                            <button className=" text-white font-[550] bg-[#1DAEFF] px-12 rounded-xl py-2">Contact Us</button>
+                <div className="rounded-3xl bg-gradient-to-b from-[#1DAEFF] to-[#ddeefc] h-[480px] flex flex-row justify-between">
+                    <div className="pl-16 pt-20">
+                        <p className="text-[54px] w-[400px] font-[600]">Find your best Real Estate</p>
+                        <p className="pt-8 w-[470px] text-[22px] font-[400] text-[#585981]">
+                            We provide a complete service for the sale, purchase or rental of real estate.
+                        </p>
+                        <div className="pt-8">
+                            <button className="text-white font-[550] bg-[#1DAEFF] px-12 rounded-xl py-2">
+                                Contact Us
+                            </button>
                         </div>
                     </div>
 
                     <div>
-                        <img src="/19.jpg" alt="" className="rounded-3xl  h-[480px] w-[605px]" />
+                        <img src="/19.jpg" alt="" className="rounded-3xl h-[480px] w-[605px]" />
                     </div>
-
                 </div>
             </div>
-
         </>
-
     );
 }
 
