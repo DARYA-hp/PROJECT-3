3ee10f7cb56d6efbbb1ce29738074954a0ace919
darya <dhaghighatpanah@gmail.com>
2025-11-09 21:46:00 +0330

    image 1 in pro page

---
+++ src/Components/Proimage.jsx
@@ -0,0 +1,14 @@
+function ProImage() {
+    return (
+        <>
+
+            <div>
+                <div>
+                    <img src="/20.jpg" alt="" className="rounded-3xl w-[860px] h-[477px]"/>
+                </div>
+            </div>
+
+        </>
+    )
+}
+export default ProImage
