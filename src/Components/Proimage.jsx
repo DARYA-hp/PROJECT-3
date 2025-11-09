cfbc46261c2847ee95895a911a1087db104d5352
darya <dhaghighatpanah@gmail.com>
2025-11-09 21:51:19 +0330

    4 image in propage

--- src/Components/Proimage.jsx
+++ src/Components/Proimage.jsx
@@ -4,7 +4,14 @@ function ProImage() {
 
             <div>
                 <div>
-                    <img src="/20.jpg" alt="" className="rounded-3xl w-[860px] h-[477px]"/>
+                    <img src="/20.jpg" alt="" className="rounded-3xl w-[860px] h-[477px]" />
+                </div>
+
+                <div className="pt-12 flex flex-row justify-between w-[860px]">
+                    <img src="/4.jpg" alt="" className="w-[199px] h-[154px] rounded-xl" />
+                    <img  src="/21.jpg" alt="" className="w-[199px] h-[154px] rounded-xl" />
+                    <img src="/22.jpg" alt="" className="w-[199px] h-[154px] rounded-xl" />
+                    <img src="/23.jpg" alt="" className="w-[199px] h-[154px] rounded-xl"/>
                 </div>
             </div>
 
