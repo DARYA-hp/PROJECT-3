29be1913a1552d25fd5d107d77feb6f107800905
darya <dhaghighatpanah@gmail.com>
2025-11-09 22:39:34 +0330

    Product created

---
+++ src/Pages/Product.jsx
@@ -0,0 +1,12 @@
+import ProDesc from "../Components/Prodesc";
+import ProImage from "../Components/Proimage";
+
+function Product(){
+    return(
+        <>
+        <ProImage/>
+        <ProDesc/>
+        </>
+    )
+}
+export default Product
