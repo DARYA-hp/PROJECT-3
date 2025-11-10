39100cfa4b24b2e99c59e871079b9a98dcbc4b8d
darya <dhaghighatpanah@gmail.com>
2025-11-10 21:58:06 +0330

    color is change

--- src/Components/Slide3.jsx
+++ src/Components/Slide3.jsx
@@ -58,7 +58,7 @@ function Slide3() {
             </div>
 
             <div className="px-28 pt-28">
-                <div className="rounded-3xl bg-gradient-to-b from-[#C3DFED] to-[#DFF0F7] h-[480px] flex flex-row justify-between">
+                <div className="rounded-3xl bg-gradient-to-b from-[#9dc8df] to-[#DFF0F7] h-[480px] flex flex-row justify-between">
                     <div className="pl-16 pt-20">
                         <p className="text-[54px] w-[400px] font-[600]">Find your best Real Estate</p>
                         <p className="pt-8 w-[470px] text-[22px] font-[400] text-[#585981]">
@@ -72,7 +72,7 @@ function Slide3() {
                     </div>
 
                     <div>
-                        <img src="/19.jpg" alt="" className="rounded-3xl h-[480px] w-[605px]" />
+                        <img src="/19.jpg" alt="" className="rounded-r-3xl h-[480px] w-[605px]  " />
                     </div>
                 </div>
             </div>
