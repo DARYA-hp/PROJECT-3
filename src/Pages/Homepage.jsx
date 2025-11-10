10f9117b860aee89f3b94abb78f6f7259eee8571
darya <dhaghighatpanah@gmail.com>
2025-11-10 22:19:36 +0330

    bg image set

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -8,7 +8,7 @@ function HomePage() {
         <>
             <div className="relative overflow-hidden">
 
-                 <img src="/green.png" alt="" className="absolute top-[620px] right-0 -z-10" />
+                 <img src="/green.png" alt="" className="absolute top-[621px] right-0 -z-10" />
                  <img src="/orange.png" alt="" className="absolute top-[1600px] left-0 -z-10" />
                  <img src="/orange.png" alt="" className="absolute top-[2770px]  -z-10" />
                  <img src="/green.png" alt="" className="absolute top-[3420px] right-0 -z-10" />
