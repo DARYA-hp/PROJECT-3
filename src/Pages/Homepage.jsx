f40fa0e1e3c60fddc9bff1ba674affaf34e12647
darya <dhaghighatpanah@gmail.com>
2025-11-10 22:27:11 +0330

    bg image in homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -8,10 +8,10 @@ function HomePage() {
         <>
             <div className="relative overflow-hidden">
 
-                 <img src="/green.png" alt="" className="absolute top-[621px] right-0 -z-10" />
-                 <img src="/orange.png" alt="" className="absolute top-[1600px] left-0 -z-10" />
-                 <img src="/orange.png" alt="" className="absolute top-[2770px]  -z-10" />
-                 <img src="/green.png" alt="" className="absolute top-[3420px] right-0 -z-10" />
+                 <img src="/green.png" alt="" className="absolute top-[560px] right-0 -z-10" />
+                 <img src="/orange.png" alt="" className="absolute top-[1500px] left-0 -z-10" />
+                 <img src="/orange.png" alt="" className="absolute top-[2670px]  -z-10" />
+                 <img src="/green.png" alt="" className="absolute top-[3350px] right-0 -z-10" />
 
                 
                 <Slide1/>
