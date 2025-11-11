83481b8d27c46ef9e1ed72717a35abdba02ba992
darya <dhaghighatpanah@gmail.com>
2025-11-11 10:16:52 +0330

    market rote added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-
+import 'leaflet/dist/leaflet.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
-import Dec from "./Pages/Dec";
-import HomePage from "./Pages/HomePage";
-
-function App() {
-  return (
+import Product from "./Pages/Product"
+import HomePage from "./Pages/Homepage"
+import Market from './Pages/Market';
+function App(){
+  return(
     <>
     <Router>
       <Routes>
         <Route path="/" element={<Layout><HomePage /></Layout>} />
-        <Route path="/dec" element={<Layout><Dec /></Layout>} />
+        <Route path="/Product" element={<Layout><Product /></Layout>} />
+        <Route path="/Market" element={<Layout><Market/></Layout>}/>
       </Routes>
     </Router>
     </>
-  )}
+  )
+}
 
 export default App;
